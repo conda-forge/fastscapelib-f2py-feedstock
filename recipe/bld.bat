c2f7060cd4402f6148152e7a4c35afde7720c065
Benoit Bovy <benbovy@gmail.com>
2020-05-19 22:54:50 +0200

    remove gfortran chocolatey + try using nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,18 +4,26 @@ dir %RECIPE_DIR%
 
 echo %LIB%
 
+:: Explicitly Set path to f2py and Fortran compiler
 if exist %PREFIX%\Scripts\f2py.exe (
   set F2PY=%PREFIX%\Scripts\f2py.exe
 ) else (
   set F2PY=%PREFIX%\Scripts\f2py.bat
 )
 
-:: Set variable required by CMake
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
+
+:: remove chocolatey gfortran
+if "%CI%" == "azure" (
+  rm "C:\ProgramData\Chocolatey\bin\gfortran.exe"
+)
 
 mkdir "%SRC_DIR%\dist"
 
-"%PYTHON%" setup.py bdist_wheel --dist-dir="%SRC_DIR%\dist" -- ^
+"%PYTHON%" setup.py bdist_wheel ^
+           --dist-dir="%SRC_DIR%\dist" ^
+           -G "NMake Makefiles" ^
+           -- ^
            -DCMAKE_Fortran_COMPILER:FILEPATH="%FC%" ^
            -DF2PY_EXECUTABLE:FILEPATH="%F2PY%"
 
