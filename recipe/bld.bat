b12122db05e3405b9095e12c6cb4eb840a012ee4
Benoit Bovy <benbovy@gmail.com>
2020-05-19 18:26:12 +0200

    try simpler win build config

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,13 +4,6 @@ dir %RECIPE_DIR%
 
 echo %LIB%
 
-:: Update PATH to ensure mingw compiler is not found
-pushd %RECIPE_DIR%
-call rmpath C:\\Program Files\\Git\\mingw64\\bin
-call rmpath C:\\ProgramData\\Chocolatey\\bin
-call rmpath C:\\Strawberry\\c\\bin
-popd
-
 if exist %PREFIX%\Scripts\f2py.exe (
   set F2PY=%PREFIX%\Scripts\f2py.exe
 ) else (
@@ -19,10 +12,6 @@ if exist %PREFIX%\Scripts\f2py.exe (
 
 :: Set variable required by CMake
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
-set CC=%BUILD_PREFIX%\Library\bin\clang-cl.exe
-set CXX=%BUILD_PREFIX%\Library\bin\clang-cl.exe
-set LIB=%LIB%;%BUILD_PREFIX%\Library\lib
-
 
 mkdir "%SRC_DIR%\dist"
 
