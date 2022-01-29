98f6fd4e102feaedf96b9eab57ce1b3fe879cded
Benoit Bovy <benbovy@gmail.com>
2022-01-29 02:10:48 +0100

    Add osx-arm64 (take 2) (#14)
    * bump build number
    
    * enable osx-arm64 + clean-up
    
    * MNT: Re-rendered with conda-build 3.21.7, conda-smithy 3.16.2, and conda-forge-pinning 2022.01.28.19.47.10
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,14 @@
-if [ $(uname) == Darwin ]; then
-    # directly setting MACOSX_DEPLOYMENT_TARGET is not supported by scikit-build
-    $PYTHON -m pip install . \
-            --global-option="build_ext" \
-            --global-option="--plat-name=macosx-$MACOSX_DEPLOYMENT_TARGET-$OSX_ARCH" \
-            --no-build-isolation \
-            --no-deps \
-            --ignore-installed \
-            --no-cache-dir \
-            -vvv
-else
-    $PYTHON -m pip install . \
-            --no-build-isolation \
-            --no-deps \
-            --ignore-installed \
-            --no-cache-dir \
-            -vvv
+#!/bin/bash
+
+set -e
+
+if [ "$target_platform" = "osx-arm64" ]; then
+  export CMAKE_OSX_ARCHITECTURES="arm64"
 fi
+
+$PYTHON -m pip install . \
+  --no-build-isolation \
+  --no-deps \
+  --ignore-installed \
+  --no-cache-dir \
+  -vvv
